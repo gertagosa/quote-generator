ae8719394da7cdfc45acfcef40233c4380ef7520
gertagosa <121582460+gertagosa@users.noreply.github.com>
2023-12-05 12:43:08 +0000

    Added basic form handling and typewrite plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: ["True friends are never apart"],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
