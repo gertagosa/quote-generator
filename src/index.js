62b1cd3473b1c9fa6fbdc0a53804bd34e9242925
gertagosa <121582460+gertagosa@users.noreply.github.com>
2023-12-07 13:32:16 +0000

    improved the User Experience

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ function generateQuote(event) {
   let context =
     "You are a quote expert and love to write short quotes.Your aim is to generate a 2 line quote in basic HTML and seperate each line with a <br/>.Make sure to follow the user instructions .";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="blink"> Generating quotes about ${instructionsTnput.value}</div>`;
   console.log("Generating quote");
   console.log(`Prompt:${prompt}`);
   console.log(`Context:${context}`);
